d5f7126a3769c5cc300bd836d8e6ef29e9e54785
jimmy-han <hgh.han@gmail.com>
2016-05-26 22:59:59 +0800

    使用ioutil.ReadFile 读取文件

---
+++ ch1/dup3/dup3.go
@@ -0,0 +1,30 @@
+// 使用ioutil.ReadFile 一次读取所有文件所有内容,使用strings.Split根据回车符分隔进行计数
+
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
+func main() {
+	counts := make(map[string]int)
+	for _, filename := range os.Args[1:] {
+		data, err := ioutil.ReadFile(filename)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "dup3: %v\n", err)
+			continue
+		}
+		for _, line := range strings.Split(string(data), "\n") {
+			counts[line]++
+		}
+	}
+
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
+}
