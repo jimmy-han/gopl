db63a9e4c014ef5643aeb4002258cbdad51fb29f
jimmy-han <hgh.han@gmail.com>
2016-06-11 23:09:06 +0800

    string 用法

---
+++ ch3/basename2/basename2.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"strings"
+	"fmt"
+)
+
+func basename(s string) string {
+	slash := strings.LastIndex(s, "/")  // -1 if '/' not found.
+	s = s[slash+1:]
+	if dot := strings.LastIndex(s, "."); dot >= 0 {
+		s = s[:dot]
+	}
+	return s
+}
+
+func main()  {
+	fmt.Println(basename("a"))
+	fmt.Println(basename("a.go"))
+	fmt.Println(basename("a/b/c.go"))
+	fmt.Println(basename("a/b.c.go"))
+}
