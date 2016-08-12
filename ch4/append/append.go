01d8848e4ff014b3d7da17f7c96f233fb6dcdd0d
jimmy-han <hgh.han@gmail.com>
2016-08-12 23:47:03 +0800

    slice append函数

--- ch4/append/append.go
+++ ch4/append/append.go
@@ -23,10 +23,36 @@ func appendInt(x []int, y int) []int {
 	return z
 }
 
+// 一次添加多个元素到slice
+func appendInts(x []int, y ...int) []int {
+	var z []int
+	zlen := len(x) + len(y)
+	if zlen <= cap(x) {
+		 z = x[:zlen]
+	} else {
+		zcap := zlen
+		if zcap < 2 * zlen {
+			 zcap = 2 * zlen
+		}
+		z = make([]int, zlen, zcap)
+		copy(z, x)
+	}
+	copy(z[len(x):], y)
+	fmt.Printf("cap=%d\t%v\n", cap(z), z)
+	return z
+}
+
 func main() {
 	var x []int
 	for i := 0; i < 10; i++ {
 		x = append(x, i)
 		fmt.Printf("%d  cap=%d\t%v\n", i, cap(x), x)
 	}
+
+	var y []int
+	y = appendInts(y, 1, 2)
+	y = appendInts(y, 3, 4, 5)
+	y = appendInts(y, 6, 7, 8)
+	y = appendInts(y, 9, 0)
+	fmt.Println(y)
 }
